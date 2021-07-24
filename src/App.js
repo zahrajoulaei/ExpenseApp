e5a807616451609382c6ce2c4858daafb5e86891
Zahra Hassanjoulaee <zahra.hassanjoulaee@s2017.tu-chemnitz.de>
2021-07-24 18:28:30 +0200

    restructure the file components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 const expenses = [
   {
@@ -22,11 +22,11 @@ const expenses = [
   },
 ];
 
-function App() {
+const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
-        <Expenses items={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 }
