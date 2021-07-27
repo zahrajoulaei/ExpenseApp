60521db3fc6ce901b5735226cdfc60d2e5b7b079
ZahraJou <zahrajoulaei@gmail.com>
2021-07-27 18:49:23 +0200

    add the form for adding new items in expense app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const expenses = [
   {
@@ -25,7 +26,7 @@ const expenses = [
 const App = () => {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense></NewExpense>
       <Expenses items={expenses} />
     </div>
   );
